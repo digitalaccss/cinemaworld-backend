14cbff8f19197e65e0050a20d4208df0ed2381f8
Mohsin Khan <mohsin@doormonk.in>
2025-01-18 03:11:09 +0530

    Add Alt Option to Film Controller

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log; 
 use Illuminate\Support\Facades\Storage;
 
 use App\Models\Show;
@@ -92,7 +93,11 @@ class FilmController extends Controller
                 $data[$indexPos]['sub_title'] = $film->sub_title;
                 $data[$indexPos]['expiry_date'] = ($film->expiry_date == null)? null: $film->expiry_date->format('Y-m-d');
                 $data[$indexPos]['is_new'] = $film->is_new;
-                
+                $data[$indexPos]['tonight_banner_alt'] = $film->tonight_banner_alt;
+                $data[$indexPos]['banner_alt'] = $film->banner_alt;
+                $data[$indexPos]['image_alt'] = $film->image_alt;
+                $data[$indexPos]['director_statement_alt'] = $film->director_statement_alt;
+        
                 $genreIDs = ShowGenre::where('show_id', $film->id)->pluck('genre_id');
                 if(count($genreIDs) >= 1){
                     $genres = Genre::whereIn('id', $genreIDs)->pluck('genre_display_name');
@@ -121,7 +126,7 @@ class FilmController extends Controller
         ->where('slug', $req->slug)
         ->where('is_publish', true)
         ->first();
-
+        
         if($film){
             $data = [];
 
@@ -141,31 +146,32 @@ class FilmController extends Controller
             $data['director_statement'] = $film->director_statement;
             $data['is_new'] = $film->is_new;
             $data['on_demand'] = $film->on_demand;
-            $data['meta_title'] = $film->meta_title;
-            $data['meta_description'] = $film->meta_description;
+            $data['meta_title'] = $film->meta_title ?? '';
+            $data['meta_description'] = $film->meta_description ?? '';
+            $data['banner_alt'] = $film->banner_alt ?? '';
+            $data['image_alt'] = $film->image_alt ?? '';            
             $data['director_statement_alt'] = $film->director_statement_alt;
             $data['tonight_banner_alt'] = $film->tonight_banner_alt;
-            $data['banner_alt'] = $film->banner_alt;
-            $data['image_alt'] = $film->image_alt;
-
+           
+             
             $genreIDs = ShowGenre::where('show_id', $film->id)->pluck('genre_id');
             if(count($genreIDs) >= 1){
                 $genres = Genre::select('genre_name', 'genre_display_name')->whereIn('id', $genreIDs)->get();
-                $data['genres'] = $genres;
+                $data['genres'] = $genres->toArray() ?? [];
             }
 
             $countryIDs = ShowCountry::where('show_id', $film->id)->pluck('country_id');
             if(count($countryIDs) >= 1){
                 $countries = Country::whereIn('id', $countryIDs)->pluck('country_display_name');
-                $data['countries'] = $countries;
+                $data['countries'] = $countries->toArray() ?? [];
             }
 
             $languageIDs = Showlanguage::where('show_id', $film->id)->pluck('language_id');
             if(count($languageIDs) >= 1){
                 $languages = Language::whereIn('id', $languageIDs)->pluck('language_display_name');
-                $data['languages'] = $languages;
+                $data['languages'] = $languages->toArray() ?? [];
             }
-
+            
             $accoladeIDs = ShowAccolade::where('show_id', $film->id)->pluck('accolade_id');
             if(count($accoladeIDs) >= 1){
                 $accolades = Accolade::select('name', 'category', 'cover_photo_path', 'cover_photo_alt')->whereIn('id', $accoladeIDs)->get();
@@ -179,17 +185,17 @@ class FilmController extends Controller
                         }
                     }
                 }
-                $data['accolades'] = $accolades;
+                $data['accolades'] = $accolades->toArray() ?? [];
             }
 
             $critics = Critic::select('name', 'critique')->where('show_id', $film->id)->get();
             if(count($critics) >= 1){
-                $data['critics'] = $critics;
+                $data['critics'] = $critics->toArray() ?? [];
             }
 
             $directorIDs = ShowDirector::where('show_id', $film->id)->pluck('director_id');
             if(count($directorIDs) >= 1){
-                $directors = Director::select('id', 'name', 'slug', 'profile_photo_path')->whereIn('id', $directorIDs)->get();
+                $directors = Director::select('id', 'name', 'slug', 'profile_photo_path', 'profile_photo_alt')->whereIn('id', $directorIDs)->get();
                 foreach($directors as $director){
                     $director->slug = '/directors/'.$director->slug;
                     if($director->profile_photo_path){
@@ -200,13 +206,21 @@ class FilmController extends Controller
                           $director->profile_photo_path = null;
                       }
                     }
-                    $data['directors'][] = $director;
+                    $data['directors'] = $directors->map(function ($director) {
+                        return [
+                            'id' => $director->id,
+                            'name' => $director->name,
+                            'slug' => $director->slug,
+                            'profile_photo_path' => $director->profile_photo_path,
+                            'profile_photo_alt' => $director->profile_photo_alt,
+                        ];
+                    })->toArray();
                 }
             }
-
+           
             $castIDs = ShowCast::where('show_id', $film->id)->pluck('cast_id');
             if(count($castIDs) >= 1){
-                $castArr = Cast::select('id', 'name', 'slug', 'profile_photo_path')->whereIn('id', $castIDs)->get();
+                $castArr = Cast::select('id', 'name', 'slug', 'profile_photo_path', 'profile_photo_alt')->whereIn('id', $castIDs)->get();
 
                 foreach($castArr as $cast){
                     $cast->slug = '/casts/'.$cast->slug;
@@ -218,8 +232,17 @@ class FilmController extends Controller
                             $cast->profile_photo_path = null;
                         }
                     }
-                    $data['casts'][] = $cast;
+                    $data['casts'] = $castArr->map(function ($cast) {
+                        return [
+                            'id' => $cast->id,
+                            'name' => $cast->name,
+                            'slug' => $cast->slug,
+                            'profile_photo_path' => $cast->profile_photo_path,
+                            'profile_photo_alt' => $cast->profile_photo_alt,
+                        ];
+                    })->toArray();
                 }
+                
             }
             
             if($film->banner_path){
@@ -230,34 +253,42 @@ class FilmController extends Controller
                     $data['banner_path'] = null;
                 }
             }
-
-            if($film->media){
-                $photoGalleryFilePath = null;
-
-                foreach($film->getMedia('film-gallery-collection') as $photoGalleryObj){
-                    $photoGalleryFileName = $photoGalleryObj->file_name;
-                    $photoGalleryFilePath = 'img/'.$photoGalleryObj->collection_name.'/'.$photoGalleryObj->id.'/'.$photoGalleryFileName;
-
-                    if(Storage::disk('public')->exists($photoGalleryFilePath)){
-                        $photoGalleryFilePath = Storage::url($photoGalleryFilePath);
-                        $data['photo_gallery_paths'][] = $photoGalleryFilePath;
+            
+            if ($film->media && count($film->media) > 0) {
+                $data['photo_gallery_paths'] = []; // Initialize
+            
+                foreach ($film->media as $mediaItem) {
+                    $photoGalleryFilePath = 'img/' . $mediaItem['collection_name'] . '/' . $mediaItem['id'] . '/' . $mediaItem['file_name'];
+            
+                    Log::info('Generated Photo Path:', ['path' => $photoGalleryFilePath]);
+            
+                    if (Storage::disk('public')->exists($photoGalleryFilePath)) {
+                        $url = Storage::url($photoGalleryFilePath);
+                        Log::info('Photo Path Exists:', ['url' => $url]);
+                        $data['photo_gallery_paths'][] = $url;
+                    } else {
+                        Log::warning('Photo Path Does Not Exist:', ['path' => $photoGalleryFilePath]);
+                        $data['photo_gallery_paths'][] = null;
                     }
                 }
+            
+                Log::info('Final Photo Gallery Paths:', $data['photo_gallery_paths']); // Final log
             }
 
             if($film->director_photo_path){
                 if(Storage::disk('public')->exists($film->director_photo_path)){
                     $data['director_photo_path'] = Storage::url($film->director_photo_path);
+                    $data['director_statement_alt'] = $film->director_statement_alt;
                 }
                 else {
                     $data['director_photo_path'] = null;
                 }
             }
-
+           
             if($genreIDs){
                 $showsWithSameGenres = ShowGenre::whereIn('genre_id', $genreIDs)->pluck('show_id');
                 if(count($showsWithSameGenres) >= 1){
-                    $youMayAlsoLikes = Show::select('id', 'title', 'foreign_title', 'slug', 'show_type_id', 'cover_photo_path', 'is_new', 'is_publish')
+                    $youMayAlsoLikes = Show::select('id', 'title', 'foreign_title', 'slug', 'show_type_id', 'cover_photo_path', 'cover_photo_alt', 'is_new', 'is_publish')
                     ->where('show_type_id', 1)
                     ->whereNot('id', $film->id)
                     ->where('is_publish', true)
@@ -266,7 +297,7 @@ class FilmController extends Controller
                     ->inRandomOrder()
                     ->limit(6)
                     ->get();
-
+               
                     if(count($youMayAlsoLikes) >= 1){
                         foreach($youMayAlsoLikes as $indexPos => $youMayAlsoLike){
                             $data['you_may_also_likes'][$indexPos]['film_id'] = $youMayAlsoLike->id;
@@ -274,7 +305,8 @@ class FilmController extends Controller
                             $data['you_may_also_likes'][$indexPos]['foreign_title'] = $youMayAlsoLike->foreign_title;
                             $data['you_may_also_likes'][$indexPos]['slug'] = '/films/'.$youMayAlsoLike->slug;
                             $data['you_may_also_likes'][$indexPos]['is_new'] = $youMayAlsoLike->is_new;
-                            
+                            $data['you_may_also_likes'][$indexPos]['cover_photo_alt'] = $youMayAlsoLike->cover_photo_alt || null;
+
                             if($youMayAlsoLike->cover_photo_path){
                                 if(Storage::disk('public')->exists($youMayAlsoLike->cover_photo_path)){
                                     $youMayAlsoLike->cover_photo_path = Storage::url($youMayAlsoLike->cover_photo_path);
@@ -286,9 +318,10 @@ class FilmController extends Controller
                             }
                         }
                     }
+                    
                 }   
             }
-
+           
             $currentDateStr = date('Y-m-d');
             $currentTimeStr = date('H:i:s');
 
@@ -328,12 +361,13 @@ class FilmController extends Controller
                 $nextScheduleTime = date('g:i A', strtotime($nextSchedule->schedule_start_time));
                 $data['next_play'] = $nextScheduleDate.' at '.$nextScheduleTime;
             }
-
+            
             // $nextScheduleDate = date('l, d M', strtotime($nextSchedule->schedule_start_date));
             // $nextScheduleTime = date('g:i A', strtotime($nextSchedule->schedule_start_time));
             // $data['next_play'] = $nextScheduleDate.' at '.$nextScheduleTime;
-            
+           
             return response()->json($data, 200);
+            
         }
         else {
             return response()->json('This film was not found', 202);
